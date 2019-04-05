c1c70048d5ca93c8651a10d46f01a10b8872b9d4
arsts <arstsu@gmail.com>
2019-04-05 21:19:37 +0300

    Day1-P3

---
+++ Day-1/Let-and-Const.js
@@ -0,0 +1,12 @@
+
+
+function main() {
+  // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+  const r = readLine();
+  const PI = Math.PI;
+  const area = PI * Math.pow(r, 2);
+  const perimeter = 2 * Math.PI * r;
+  // Print the area of the circle:
+  console.log(area)
+  // Print the perimeter of the circle:
+  console.log(perimeter)
